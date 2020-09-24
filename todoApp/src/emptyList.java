95545b371b009e1900d17c552d87e390b257ef97
Marketa Hajickova <marketahajickova@gmail.com>
2020-09-24 11:59:03 +0200

    Exercise EmptyList

---
+++ todoApp/src/emptyList.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class emptyList {
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      System.out.println("No given argument!");
+      return;
+    }
+    switch (args[0]) {
+      case "-l":
+        Path path = Paths.get("src/data/emptyfile.txt");
+        try {
+          List<String> lines = Files.readAllLines(path);
+          int count = 0;
+          if (lines.size() == 0) {
+            System.out.println("No todos for today! :) ");
+          } else {
+            for (String line : lines) {
+              count++;
+              System.out.println(count + " - " + line);
+            }
+          }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        break;
+      default:
+        System.out.println("Not valid argument!");
+        break;
+    }
+  }
+}
